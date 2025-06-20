37107c69b3e75a8d6d74eb6ba781955b2525bcca
Diyor <baxacap@gmail.com>
2025-06-20 18:05:34 +0500

    navbar 100%

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Navbar.scss";
+import GridViewIcon from "@mui/icons-material/GridView";
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
+import ImportantDevicesIcon from '@mui/icons-material/ImportantDevices';
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [activeLink, setActiveLink] = useState("catalog");
 
-export default Navbar
+  const links = [
+    { name: "Catalog", key: "catalog", icon: <GridViewIcon /> },
+    { name: "Configurator", key: "configurator", icon: <SettingsSuggestIcon/> },
+    { name: "Buy Computer", key: "buy compputer", icon: <ImportantDevicesIcon /> },
+    { name: "News", key: "news" },
+    { name: "HyperX", key: "hyperx" },
+    { name: "All Brands", key: "all" },
+  ];
+
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        {links.map((link) => (
+          <li
+            key={link.key}
+            className={`nav-item ${activeLink === link.key ? "active" : ""}`}
+            onClick={() => setActiveLink(link.key)}
+          >
+            <a href="#">
+              {link.icon}
+              {link.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
