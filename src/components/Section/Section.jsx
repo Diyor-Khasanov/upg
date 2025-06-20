6d9538ca54a174bbac04d482879a583f31bbfc00
Diyor <baxacap@gmail.com>
2025-06-20 19:10:46 +0500

    section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./Section.scss";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const Section = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="hero">
+      <div className="hero__container">
+        <div className="hero__text">
+          <h1 className="hero__title">Configurator</h1>
+          <p className="hero__description">
+            The UPGrade system block configurator will help you select a PC kit
+            and check the kit for compatibility. Choose, compare
+            characteristics, order additional options to learn how to create
+            your own configuration!
+          </p>
+          <button className="hero__btn">
+            Pick Up a PC
+            <ArrowForwardIcon />
+          </button>
+        </div>
 
-export default Section
+        <div className="hero__image">
+          <img src="https://india.gccgamers.com/medium-gaming-pcs/assets/product.webp" alt="Hero" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Section;
